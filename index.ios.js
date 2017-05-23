ed0a8aa2ff15084e7ee6349ff42c0f12f2ae69c9
shivashp <shivapandey04@gmail.com>
2017-05-23 21:29:53 +0545

    Call

--- index.ios.js
+++ index.ios.js
@@ -9,15 +9,15 @@ import React, { Component } from 'react';
 import {
   AppRegistry, StatusBar, View
 } from 'react-native';
-// import Root from './src/Root';
-import App from './src/App';
+// import App from './src/App';
+import PhoneProfile from './src/components/PhoneProfile';
 
 export default class Roomie extends Component {
   render() {
     return (
       <View style={{flex:1}}>
         <StatusBar hidden={true} />
-        <App />
+        <PhoneProfile />
       </View>
     );
   }
---
+++ src/components/PhoneProfile.js
@@ -0,0 +1,87 @@
+import React, { PropTypes } from 'react'
+import {
+  View, Text, StyleSheet, Image, TouchableOpacity
+} from 'react-native';
+import * as global from '../styles/global';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+class PhoneProfile extends React.Component {
+  render () {
+    return(
+      <Image
+        source={{uri: 'http://keenthemes.com/preview/metronic/theme/assets/pages/media/profile/profile_user.jpg'}}
+        style={[global.COMMON.container,{width:'100%', height:undefined}]}
+        >
+        <View style={styles.overlay}></View>
+        <View style={styles.topSection}>
+          <Text style={styles.name}>
+            SHIVA PANDEY
+          </Text>
+        </View>
+        <View style={styles.bottomSection}>
+          <TouchableOpacity style={[styles.roundButton,styles.btnRed]}>
+            <Icon style={global.COMMON.icon} name="ios-close" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.roundButton}>
+            <Icon style={global.COMMON.icon} name="ios-volume-up-outline" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.roundButton}>
+            <Icon style={global.COMMON.icon} name="ios-mic-outline" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.roundButton}>
+            <Icon style={global.COMMON.icon} name="ios-videocam-outline" />
+          </TouchableOpacity>
+        </View>
+      </Image>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#efefef",
+    padding:20,
+  },
+  overlay: {
+    position: 'absolute',
+    top:0,
+    left:0,
+    width:'100%',
+    height: '100%',
+    backgroundColor: 'rgba(0,0,0,0.3)',
+  },
+  topSection: {
+    flex:2,
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  bottomSection: {
+    flex:1,
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  name: {
+    paddingVertical:30,
+    backgroundColor: 'transparent',
+    fontSize:33,
+    color: 'white',
+  },
+  roundButton: {
+    backgroundColor: '#5C6BC0',
+    width:50,
+    height:50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 50,
+    marginHorizontal:13,
+    marginTop:50
+  },
+  btnRed: {
+    backgroundColor: 'red'
+  },
+
+})
+
+
+export default PhoneProfile;
